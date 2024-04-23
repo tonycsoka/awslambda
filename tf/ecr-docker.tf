26c089b37ac1c63538316e677253ec405e01b374
Tony Csoka <Tony.Csoka@gmail.com>
2024-04-23 07:55:01 +0100

    deployment stuf

---
+++ tf/ecr-docker.tf
@@ -0,0 +1,18 @@
+resource "aws_ecr_repository" "app_ecr_repo" {
+  name = "app-repo"
+}
+
+resource "null_resource" "docker_packaging" {
+  triggers = {
+    "run_at" = timestamp()
+    files = "${filebase64sha256("../Dockerfile")}"
+  }
+  provisioner "local-exec" {
+    command = <<EOF
+aws ecr get-login-password --region ${var.aws_region} | docker login --username AWS --password-stdin ${aws_ecr_repository.app_ecr_repo.repository_url}
+docker build -t ${aws_ecr_repository.app_ecr_repo.repository_url}:latest ..
+docker push ${aws_ecr_repository.app_ecr_repo.repository_url}:latest
+EOF
+  }
+}
+
